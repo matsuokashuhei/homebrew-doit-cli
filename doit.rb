2fe104200740879eb2309bab38f4be179e09ca9c
Shuhei Matsuoka <matsuokashuheiii@gmail.com>
2025-08-24 15:14:28 +0900

    Update doit.rb

--- doit.rb
+++ doit.rb
@@ -2,29 +2,29 @@ class Doit < Formula
   desc "A CLI progress monitor for time-based visualization"
   homepage "https://github.com/matsuokashuhei/doit"
   license "MIT"
-  version "0.8.1"
+  version "0.8.2"
 
   on_macos do
     if Hardware::CPU.intel?
       url "https://github.com/matsuokashuhei/doit/releases/download/v0.8.1/doit-macos-x86_64"
-      sha256 "8321664c68e03b5740313f3d34db00e9b1fbb1a131cee46cb2ab37a2d14622ff"
+      sha256 "18ba9d5b880a0fe8e89ab9de58b40a79069472417a3a2e47e7b498abf9505f45"
     end
 
     if Hardware::CPU.arm?
       url "https://github.com/matsuokashuhei/doit/releases/download/v0.8.1/doit-macos-aarch64"
-      sha256 "1599c42330fad77628596248a09eabf53f055296ac012026cd043be5f4a35aba"
+      sha256 "786a4932956bdc0fb2920bfd139496f5694a77a4a58448c1687b64bafbae69e0"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
       url "https://github.com/matsuokashuhei/doit/releases/download/v0.8.1/doit-linux-x86_64"
-      sha256 "3dfd74d5b7e66a6a5514d6a687af2fa371ed418833105b1fd1b1e8d20c9e82dd"
+      sha256 "4a3fdc947581600ee3ea28566885f0106210a9ada9534e2b3400b136f23a7ec9"
     end
 
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
       url "https://github.com/matsuokashuhei/doit/releases/download/v0.8.1/doit-linux-aarch64"
-      sha256 "f59407364bd5a6734ca28e2eb6291d8c14a2e07fa800b9fc7acc3d432e8c75d7"
+      sha256 "b6dd04ef5526e257ea08faf3f2357cd6dadfa39d6358b87fab376e9a25c39218"
     end
   end
 
